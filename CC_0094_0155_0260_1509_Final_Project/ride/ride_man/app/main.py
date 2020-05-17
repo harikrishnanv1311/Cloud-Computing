0f79d52df877d9fb7e0b9ea05b9fa727e6ccb2c7
kruthik-jt <36992052+kruthik-jt@users.noreply.github.com>
2020-05-17 23:17:57 +0530

    Documented rides

--- CC_0094_0155_0260_1509_Final_Project/ride/ride_man/app/main.py
+++ CC_0094_0155_0260_1509_Final_Project/ride/ride_man/app/main.py
@@ -8,28 +8,38 @@ import csv
 import datetime
 import json
 
-# path = "app.db"
+#IP addresses of instances
 areapath = "AreaNameEnum.csv"
 ipaddr = "http://35.169.72.238"
-# ipaddr="http://10.0.2.15:7000"
 ipaddr_user = "http://34.236.8.161"
 ipaddr_ride = "http://3.208.45.172"
 
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app) #to enable cross domain requests
 
 #########################################################################################
+#Certain flags are used throughout the code to identify the requests to a particular category
+#join=0 ==> create a new user
+#join=1 ==> a user wants to join a ride
+#join=2 ==> delete a ride
 
-#dual_request_flag => 1 -> read should be counted , 0 -> read shouldnt be counted
+#where=0 ==> The sql query to be executed on the database doesn't have a 'where' clause
+#where=1 ==> The sql query to be executed on the database has a 'where' clause
+
+#dual_request_flag is used to avoid counting internal requests sent to the database and only
+#to count the requests sent by the user. This avoids considering internal requests as actual
+#requests and avoid unnecessary scaling
+#dual_request_flag=1 ==> read should be counted as a request sent by the client
+#dual_request_flag=0 ==> read shouldnt be counted as a request sent by the client(internal request)
 #########################################################################################
+
+
+#Function checks if the data received as input is a time in the future
 def check_date(date):
-    #29-01-2020:30-20-18
-    ##print(date)
     rem_quotes=date.replace("'","")
     rep_date=rem_quotes.replace(":","-").split("-")
-    ##print(rep_date)
     rep_date1=[int(i) for i in rep_date]
     fin_date=datetime.datetime(rep_date1[2],rep_date1[1],rep_date1[0],rep_date1[5],rep_date1[4],rep_date1[3])
     cur_date=datetime.datetime.now()
@@ -40,36 +50,26 @@ def check_date(date):
 
 
 
-
+# API to join an existing ride
 @app.route("/api/v1/rides/<rideId>",methods=["POST"])
 def join_ride(rideId):
 
-    # with open("count.json", "r") as jsonFile:
-    #     data = json.load(jsonFile)
-
-    # temp = data["count"]
-    # temp=temp+1
-    # data["count"] = temp
-
-    # with open("count.json", "w") as jsonFile:
-    #     json.dump(data, jsonFile)
-    
-
     username=request.get_json()["username"]
     fetch_data = {"table":"users","insert":["username"],"where_flag":0,"dual_request_flag":0}
+    #request sent to check if user exists
     x=requests.post(ipaddr+"/api/v1/db/read",json=fetch_data)
     if x.status_code==204:return Response(status=400)
     l1 = x.text.split(",")
     print(l1)
     if(username not in l1):
-        print("")
         return Response(status=400)
     data = {"table":"rides","rideId":rideId,"username":username,"join":1}
+    #request sent to join the ride with a particular rideId
     req = requests.post(ipaddr+"/api/v1/db/write",json=data)
     print("user joined ")
     return Response(status=int(req.text))
 
-
+#API to create a ride
 @app.route("/api/v1/rides",methods=["POST"])
 def create_ride():
     
@@ -77,49 +77,43 @@ def create_ride():
         dicts={}
         try:
             with open(areapath,'r') as csvfile:
-                # print("Got Data")
                 c = csv.reader(csvfile)
                 for row in c:
-                    dicts[row[0]]=row[1]
+                    #dictionary containing the mapping between area_name and area_code
+                    dicts[row[0]]=row[1] 
             
-            #print(dicts)
-            #print(type(request.get_json()))
+            
             created_by=request.get_json()["created_by"]
             fetch_data = {"table":"users","insert":["username"],"where_flag":0,"dual_request_flag":0}
 
             x=requests.post(ipaddr+"/api/v1/db/read",json= fetch_data)
-            print("yolo")
             if x.status_code==204:return Response(status=400)
-            print("yolo2")
             l1= x.text.split(",")
-            # l1 = s.split("\n")
-            # print("#########")
             print(l1)
-            # print("#########")
             if(created_by not in l1):
                 return Response(status=400)
             timestamp=request.get_json()["timestamp"]
-            #print("2")
             source=request.get_json()["source"]
-            #print("3")
             destination=request.get_json()["destination"]
-            #print(timestamp)
+            #checking the format of timestamp
             if(time.strptime(timestamp, "%d-%m-%Y:%S-%M-%H")):
-                #print("correct format")
                 data = {"join":0,"table":"rides","created_by":created_by,"timestamp":timestamp,"source":dicts[source],"destination":dicts[destination]}
-                #print(data)
+                #request sent to create a new ride
                 req = requests.post(ipaddr+"/api/v1/db/write",json=data)
                 return Response(status=int(req.text))
 
         except Exception as e:
                 return Response(status=400)
     else:
-        return Response(status=405)    
+        return Response(status=405)    #wrong method used
 
+
+#API to list all the details of a particular ride
 @app.route("/api/v1/rides/<rideId>",methods=["GET"])
 def list_details(rideId):
     
     if(request.method=="GET"):
+        #checking if ride with rideId exists
         check_ride={"table":"rides","insert":["rideId"],"where_flag":1,"where":"rideId="+rideId,"dual_request_flag":1} 
         req_ride=requests.post(ipaddr+"/api/v1/db/read",json=check_ride)
         response_from_api=req_ride.text
@@ -129,24 +123,25 @@ def list_details(rideId):
         csvfile=open(areapath,'r')
         c = csv.reader(csvfile)
     
-        ##print(fields)
+
         for row in c:
             dicts[row[0]]=row[1]
         if(len(resp1)>1):
-
+            #request sent to fetch ride details
             data={"table":"rides","insert":["rideId","created_by","ride_users","timestamp","source","destination"],"where":"rideId="+rideId,"where_flag":1,"dual_request_flag":0}
             req=requests.post(ipaddr+"/api/v1/db/read",json=data)
         
             response_from_api=req.text
             resp1=response_from_api.split("\n")
-            #print(resp1)
             for resp in resp1:
                 resp2=resp[1:-1].split(",")
                 users=",".join(resp2[2:len(resp2)-3])
                 pt=len(resp2)-4
-                src=list(dicts.keys())[list(dicts.values()).index(resp2[pt+2][1:].replace("'",""))]
-                dst=list(dicts.keys())[list(dicts.values()).index(resp2[pt+3][1:].replace("'",""))]
-                
+                #retreiving the area_code using area_name
+                src=list(dicts.keys())[list(dicts.values()).index(resp2[pt+2][1:].replace("'",""))] 
+                #retreiving the area_code using area_name
+                dst=list(dicts.keys())[list(dicts.values()).index(resp2[pt+3][1:].replace("'",""))] 
+            
                 if(len(resp2)>1):
                     return jsonify(
                         rideId=int(resp2[0].replace("'","").replace(" ","")),
@@ -162,9 +157,9 @@ def list_details(rideId):
         else:
             return Response(status=400)
     else:
-        return Response(status=405)
+        return Response(status=405) #wrong method used
 
-#200,204,400,405
+#API to list the ride details of all the rides from a source to a destination
 @app.route("/api/v1/rides",methods=["GET"])
 def list_source_to_destination():
 
@@ -172,7 +167,6 @@ def list_source_to_destination():
 
         source=request.args.get('source',None)
         destination=request.args.get('destination',None)
-        print("FINALLY INSIDE!")
         print(source)
         fields=[]
         rows=[]
@@ -181,24 +175,21 @@ def list_source_to_destination():
         try:
             with open(areapath,'r') as csvfile:
                 c = csv.reader(csvfile)
-    
-        ##print(fields)
                 for row in c:
                     dicts[row[0]]=row[1]
                 source_place=dicts[source]
                 destination_place=dicts[destination]
         except KeyError:
-            #print("source or destination doesnt exist")
-            return Response(status=400)
+            return Response(status=400)#if the area_code is not within the range
     
         data={"table":"rides","insert":["rideId","created_by","ride_users","timestamp","source","destination"],"where":"source='"+source_place+"' AND destination='"+destination_place+"'","where_flag":1,"dual_request_flag":1}
         print(data)
+        #request sent to receive all the rides
         req = requests.post(ipaddr+"/api/v1/db/read",json=data)
         print("Request's reply :",req)
         response_from_api=req.text
         resp1=response_from_api.split("\n")
-        ##print(resp1)
-        
+        #if no rides exist between source and destination
         if(len(resp1)==1):
             print("Resp1:",resp1)
             return jsonify(),204 
@@ -210,16 +201,14 @@ def list_source_to_destination():
         for resp in resp1:
             resp2=resp[1:-1].split(",")
             
-            
+            #checking if rides from source and destination are upcoming or not
             if(len(resp2)>1 and check_date(resp2[len(resp2)-3])):
-                ##print("content present")
                 users=",".join(resp2[2:len(resp2)-3])
                 print(users)
-                pt=len(resp2)-4
+                pt=len(resp2)-4 #extracting ride users
                 src=list(dicts.keys())[list(dicts.values()).index(resp2[pt+2][1:].replace("'",""))]
                 dst=list(dicts.keys())[list(dicts.values()).index(resp2[pt+3][1:].replace("'",""))]
-                #s=s+"{\n rideId : "+resp2[0]+"\n created_by : "+resp2[1]+"\n ride_users : "+users+"\n timestamp : "+resp2[pt+1]+"\n source : "+src+"\n destination : "+dst+"\n }"+",\n"
-                #fin_list.append("{\n rideId : "+resp2[0]+"\n created_by : "+resp2[1]+"\n ride_users : "+users+"\n timestamp : "+resp2[pt+1]+"\n source : "+resp2[pt+2]+"\n destination : "+resp2[pt+3]+"\n }")
+                #if ride has multiple riders
                 if len(users.replace("'","").replace(" ","").split(","))!=0:
                     fin_list.append({
                     "rideId":int(resp2[0].replace("'","").replace(" ","")),
@@ -229,6 +218,7 @@ def list_source_to_destination():
                     "source":str(src),
                     "destination":str(dst)
                     })
+                #if ride has a single rider    
                 else:
                     fin_list.append({
                 "rideId":int(resp2[0].replace("'","").replace(" ","")),
@@ -241,42 +231,43 @@ def list_source_to_destination():
 
         if(len(fin_list)==0):
             print("fin_list:",fin_list)
-            return jsonify(),204 
+            return jsonify(),204 #if rides dont exist from source to destination
         else:    
-            #return s[:-2]+"\n]"
-            #print(fin_list)
             return jsonify(fin_list),200   
     else:
-        return Response(status=405) 
+        return Response(status=405) #wrong method used
 
-################################things to be added#####################################
+#API to delete a ride with a particluar rideId
 @app.route("/api/v1/rides/<rideId>",methods=["DELETE"])
 def del_ride(rideId):
     
     if(request.method=='DELETE'):
-        data = {"rideId":rideId,"join":2}
-        req = requests.post(ipaddr+"/api/v1/db/write",json=data)
+        #join=2 flag indicates a delete operation to the database
+        data = {"rideId":rideId,"join":2} 
+        #request to delete the ride with rideId=rideId
+        req = requests.post(ipaddr+"/api/v1/db/write",json=data) 
         return Response(status=int(req.text))
     else:
-        return Response(status=405)
+        return Response(status=405) #wrong method used
 
 
 
 
 #######################################################################################
 
-#######################################################################################
-
+#API to count the number of rides
 @app.route("/api/v1/rides/count",methods=["GET"])
 def ride_count():
     
     if(request.method=="GET"):
+
         data = {"table":"rides", "insert":"count(*)", "where_flag" : 0, "dual_request_flag":1}
-        req = requests.post(ipaddr+"/api/v1/db/read",json=data)
+        #request to retreive the number of rides
+        req = requests.post(ipaddr+"/api/v1/db/read",json=data) 
         r=req.text[:-1]
         return int(r)
     else:
-        return Response(status=405)
+        return Response(status=405) #wrong method used
 
 
 #######################################################################################
