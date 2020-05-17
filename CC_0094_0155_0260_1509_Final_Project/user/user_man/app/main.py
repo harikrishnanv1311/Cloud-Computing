094f08ae978ef3d4d1838f03249030e71a18e7ad
RahulPandia <55227392+RahulPandia@users.noreply.github.com>
2020-05-17 23:11:00 +0530

    Update main.py

--- CC_0094_0155_0260_1509_Final_Project/user/user_man/app/main.py
+++ CC_0094_0155_0260_1509_Final_Project/user/user_man/app/main.py
@@ -8,14 +8,21 @@ import csv
 import datetime
 import json 
 
-
+# CSV file provided to map the names with their respective numbers 
 areapath = "AreaNameEnum.csv"
+
+# the ip addresses of the instances containing the orchestrator , user and ride respectively 
 ipaddr = "http://35.169.72.238"
 ipaddr_user = "http://34.236.8.161"
 ipaddr_ride = "http://3.208.45.172"
 
+
+# Intialisation of Flask "app"
 app = Flask(__name__)
+
+# To support Cross Origin Resorce Sharing 
 CORS(app)
+
 
 
 # Function to check date
@@ -41,9 +48,9 @@ def check_date(date):
 def add_user():
     if(request.method=="PUT"):
         try:
-            print("LOL")
+            
+            #Receiving the username and pasword 
             username=request.get_json(force=True)["username"]
-            print("yo")
             password=request.get_json()["password"]
             print(type(password))
             string=password.lower()
@@ -52,11 +59,18 @@ def add_user():
             print(x)
             flag=len(x)
             print("HERE",flag,len(password),username)
+            
+            # To check whether the given details meet the criteria , i.e , password field being a SHA1 hash hex and username field is not blank
             if(flag==0 and len(password)==40 and username!=""):
                 print("In")
+                
+                # The user name and password are stored as json in the variable 'data' and are sent to the orchestrator
+                # the join:0 indicates our write api in worker.py that it is a write request made by user and thus user tables needs to be updated
                 data={"table":"users","username":username,"password":password,"join":0}
                 print("Json Data:",data)
                 print("Request being sent is:",ipaddr+"/api/v1/db/write")
+                
+                #The 'req' stores the response sent by the orchestrator 
                 req=requests.post(ipaddr+"/api/v1/db/write",json=data)
                 print("In after request")
                 return Response(status=int(req.text))
@@ -79,7 +93,7 @@ def add_user():
 @app.route("/api/v1/users/<username>",methods=["DELETE"])
 def del_users(username):
     if(request.method=="DELETE"):
-        #print("entering here")
+       # the join:3 indicates our write api in worker.py that it is a write request made by user to delete an existing user and thus user tables needs to be updated
         data={"username":username,"join":3}
         req = requests.post(ipaddr+"/api/v1/db/write",json=data)
         return Response(status=int(req.text))
@@ -97,6 +111,8 @@ def list_users():
         s=[]
         print("check")
         try:
+            # the where_flag indicates whether the request made by the container contains a 'WHERE' clause ; where_flag :0 indicating 'WHERE' clause not used and where_flag:1 indicating 'WHERE' clause used
+            # the dual_request_flag is to check whether the read request should be counted or not for scaling the slaves ; 1 indicating it should be counted for scaling and 0 indicating it should not be counted for scaling  
             data={"table":"users","insert":["username"],"where_flag":0,"dual_request_flag":1}
             req = requests.post(ipaddr+"/api/v1/db/read",json=data)
             return jsonify(req.text.split(","))
